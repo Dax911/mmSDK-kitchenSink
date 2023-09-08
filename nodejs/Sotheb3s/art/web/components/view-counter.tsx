98da29f14df0c1372e35238687d2df7bfeb7970b
Dax <haydenaylor911@gmail.com>
2023-09-07 20:56:47 -0500

    Update view-counter.tsx

--- nodejs/Sotheb3s/art/web/components/view-counter.tsx
+++ nodejs/Sotheb3s/art/web/components/view-counter.tsx
@@ -1,8 +1,4 @@
 import { kv } from '@vercel/kv'
-export const KV_URL="redis://default:b798a1b6e736423485174e66ab1fe37b@concrete-sponge-37310.kv.vercel-storage.com:37310"
-export const KV_REST_API_URL="https://concrete-sponge-37310.kv.vercel-storage.com"
-export const KV_REST_API_TOKEN="AZG-ASQgODY0YjZmYTktMWQ4OS00YjM0LWFmYjktMGE1MDQ0ZmQ4ZGI1Yjc5OGExYjZlNzM2NDIzNDg1MTc0ZTY2YWIxZmUzN2I="
-export const KV_REST_API_READ_ONLY_TOKEN="ApG-ASQgODY0YjZmYTktMWQ4OS00YjM0LWFmYjktMGE1MDQ0ZmQ4ZGI1v0XVv27nbSaV9r1e6sCbz5dl1FzOj7HGdvW53QREC64="
 
 export async function ViewCounter() {
   const views = await kv.incr('views')
